ce63bb360458bc35ee378a48dc166ae91829c23c
Mehemmed <yerivar0@gmail.com>
2025-10-14 15:55:32 +0400

    user dto done

--- src/main/java/com/app/controller/UserController.java
+++ src/main/java/com/app/controller/UserController.java
@@ -1,10 +1,11 @@
 package com.app.controller;
 
+import com.app.dto.UserDto;
 import com.app.model.User;
 import com.app.service.UserService;
-import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +18,23 @@ import java.util.List;
 public class UserController {
     private final UserService userService;
 
+
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
     @GetMapping("/getAllUsers")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<List<User>> getAllUsers(){
-        return  ResponseEntity.ok(userService.findAll());
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        return ResponseEntity.ok(userService.findAll());
+    }
+
+
+    @GetMapping("/profile")
+    public UserDto getMyProfile(@AuthenticationPrincipal User currentuser) {
+        return userService.getMyProfile(currentuser);
+
+
     }
 }
+
--- src/main/java/com/app/service/UserService.java
+++ src/main/java/com/app/service/UserService.java
@@ -1,9 +1,12 @@
 package com.app.service;
 
+import com.app.dto.UserDto;
 import com.app.model.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 import java.util.List;
 
 public interface UserService {
-   List<User> findAll();
+   List<UserDto> findAll();
+   UserDto getMyProfile( User currentuser);
 }
