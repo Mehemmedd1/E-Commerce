b7bf27612f13b05d3667b94a680c7824b3f3ede7
Mehemmed <yerivar0@gmail.com>
2025-10-17 19:46:47 +0400

    product delete

--- src/main/java/com/app/config/SecurityConfig.java
+++ src/main/java/com/app/config/SecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity // @PreAuthorize-ı aktivləşdirir
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
--- src/main/java/com/app/service/ProductServiceImpl.java
+++ src/main/java/com/app/service/ProductServiceImpl.java
@@ -1,4 +1,26 @@
 package com.app.service;
 
-public class ProductServiceImpl {
+import com.app.model.Product;
+import com.app.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
 }
